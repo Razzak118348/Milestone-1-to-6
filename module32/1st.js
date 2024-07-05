96c156f1685e5cba5f06928e785d72757266eced
razzak118348 <abdurrazzak118348@gmail.com>
2024-07-05 07:10:31 +0600

    milestone 1 to 6

---
+++ module32/1st.js
@@ -0,0 +1,14 @@
+// non primitive data 
+
+let x={job:'web developer '}
+let y=x;
+y={id:2323};
+console.log(x,y)//full change korse y er value..
+
+let p={job:'web developer '}
+let q=p;
+q.job="front end developer";//2 tar value e change hoiya jaibo 
+
+console.log(p,q)
+//ekane p ar q duitar man e change hoiya jaibo..because tara ekta location k ditect kortese ..
+
